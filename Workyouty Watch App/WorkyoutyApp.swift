2cd5e3c41eb1379ee83d8d06ccae9e24a5f3444e
Robin Heathcote <robin.heathcote@icloud.com>
2024-05-10 20:11:32 +0100

    Initial Commit

---
+++ Workyouty Watch App/WorkyoutyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkyoutyApp.swift
+//  Workyouty Watch App
+//
+//  Created by Robin Heathcote on 10/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Workyouty_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
